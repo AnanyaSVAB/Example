11c1e5274151ac9c225fd597359a5a2724f8d982
AnanyaSVAB <anusvab@gmail.com>
2023-12-14 13:47:46 +0530

    changes made in master

---
+++ src/com/User.java
@@ -0,0 +1,21 @@
+package com;
+
+class User {
+	private int userId;
+	private String userName;
+	private String userEmail;
+	private String userPassword;
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setterEmail(String userEmail) {
+		this.userEmail=userEmail;
+	}
+	public String getuserPassword() {
+		return userPassword;
+	}
+	public void setuserPasssword(String userPassword) {
+		this.userPassword=userPassword;
+	}
+}
